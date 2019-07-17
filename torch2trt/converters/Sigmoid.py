382156dc52d98ef81d279d016c21d8629f837c19
binzh93 <binzh93@gmail.com>
2019-07-17 16:53:37 +0800

    add Sigmoid.py

---
+++ torch2trt/converters/Sigmoid.py
@@ -0,0 +1,11 @@
+from torch2trt.torch2trt import *
+  
+
+@tensorrt_converter('torch.nn.Sigmoid.forward')
+def convert_Sigmoid(ctx):
+    input = ctx.method_args[1]
+    output = ctx.method_return
+
+    layer = ctx.network.add_activation(
+        input=input._trt, type=trt.ActivationType.SIGMOID)
+    output._trt = layer.get_output(0)
